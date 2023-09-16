52b884b57a45746ba12087f198a2320c9d63c013
LizaSmeh <Lizsurprise@yandex.ru>
2023-09-16 16:48:17 +0300

    Итоговый вариант

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -4,83 +4,61 @@ function getArrayParams(...arr) {
   let sum = arr[0];
    min = Math.min(...arr);
    max = Math.max(...arr);
-   sum = arr.reduce(function (x, y) {
-   return x + y;
-}, 0);
+   sum = arr.reduce((acc, item) => acc + item, 0);
    let avg = Number((sum / arr.length).toFixed(2));
    return { min: min, max: max, avg: avg };
  }
 
 function summElementsWorker(...arr) {
+  if(arr.length === 0) return 0;
   for (let i = 0; i < arr.length; i++) {
-      if (arr[i] !== undefined){
-           let sum = arr.reduce(function (x, y) {
-      return x + y;
-  }, 0);
-  return sum;}else{
-return 0;
-  }
-}
-}
+    return arr.reduce((acc, item) => acc + item, 0);
+        }
+    }
 
 function differenceMaxMinWorker(...arr) {
-  for (let i = 0; i < arr.length; i++) {
-      if (arr[i] !== undefined){
- let difference = Math.max(...arr) - Math.min(...arr);
- return difference;
-}else{
-  return 0;
-
-}};
-}
+  if(arr.length === 0) return 0;
+  return Math.max(...arr) - Math.min(...arr);
+  }
 
 function differenceEvenOddWorker(...arr) {
   let sumEvenElement = 0;
 let sumOddElement = 0;
-
+if(arr.length === 0) return 0;
 for (let i = 0; i < arr.length; i++) {
-      if (arr[i] !== undefined){
     if (arr[i] % 2 == 0) {
       sumEvenElement += arr[i];
     } else {
       sumOddElement += arr[i];
     }
-      }else{
-          return 0;
-      }
-
-              };
+             }
 
 return sumEvenElement - sumOddElement;
-
 }
 
 function averageEvenElementsWorker(...arr) {
 let sumEvenElement = 0;
 let countEvenElement = 0;
+if(arr.length === 0) return 0;
 for (let i = 0; i < arr.length; i++) {
-  if (arr[i] !== undefined) {
-  if (arr[i] % 2 == 0) {
+    if (arr[i] % 2 == 0) {
       sumEvenElement += arr[i];
       countEvenElement += 1;
-  } }else{
-      return 0;
-  }
+  } 
 };
 return (sumEvenElement / countEvenElement);
 }
 
 function makeWork (arrOfArr, func) {
-let maxWorkerResult = -Infinity;
-let array = [];
-let maxOfTheMax = [];
-for(let i; i< arrOfArr.length; i++){
-    array.push(arrOfArr[i]);
-  maxOfTheMax.push(func(...maxWorkerResult));
-
+  let maxWorkerResult = -Infinity;
+  if(arrOfArr.length === 0) return 0;
+  for(let i = 0; i< arrOfArr.length; i++){
+    const result = func(...arrOfArr[i]);
+    if(result > maxWorkerResult) maxWorkerResult = result;
 };
-
-const max = maxOfTheMax.reduce((a, b) => Math.max(a, b), -Infinity);
-return (max);
-
+  return maxWorkerResult;
 }
+
+
+
+
