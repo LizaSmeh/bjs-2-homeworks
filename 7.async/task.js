9fd753aa2d7449beaaefa4fc31b06c7b189f38b0
LizaSmeh <Lizsurprise@yandex.ru>
2023-10-07 14:05:34 +0300

    Седьмое задание полностью готово

---
+++ 7.async/task.js
@@ -0,0 +1,46 @@
+class AlarmClock {
+    constructor(){
+       this.alarmCollection = [];
+       this.intervalId = null;
+    }
+
+addClock(time, callback){
+if(time !== null || callback !== null){
+    throw new Error('Отсутствуют обязательные аргументы');
+}else if ( this.alarmCollection.find(call => call.time === time) ) {
+    console.warn('Уже присутствует звонок на это же время');
+  }
+  this.alarmCollection.push({time, callback, canCall: true});
+}
+
+removeClock(time){
+    this.alarmCollection = this.alarmCollection.filter(call => call.time !== time);
+}
+getCurrentFormattedTime(){
+    return new Date().toTimeString().slice(0, 5); 
+}
+start(){
+    if(this.intervalId !== null){
+        return;
+    }
+    this.intervalId = newInterval( () => {
+        this.alarmCollection.forEach(call => {
+          if (call.time === this.getCurrentFormattedTime() && call.canCall) {
+            call.canCall = false;
+            call.callback();          
+          }
+        })
+      }, 1000);
+}
+stop(){
+    clearInterval(this.intervalId);
+    this.intervalId = null;
+}
+resetAllCalls(){
+    this.alarmCollection.forEach(call => call.canCall = true);
+}
+clearAlarms(){
+this.stop();
+this.alarmCollection = [];
+}
+}
